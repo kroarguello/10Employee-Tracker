d8eb02bce3edb3f4191d193fb57faee6c0d541c3
Delmy Arguello <kroarguelloadel@gmail.com>
2020-10-24 09:12:30 -0500

    adding Files

--- department.sql
+++ department.sql
@@ -18,3 +18,32 @@
   * **last_name** - VARCHAR(30) to hold employee last name
   * **role_id** - INT to hold reference to role employee has
   * **manager_id** - INT to hold reference to another employee that manager of the current employee. This field may be null if the employee has no manager
+
+
+DROP DATABASE IF EXISTS department_db;
+CREATE DATABASE department_db;
+USE department_db;
+
+CREATE TABLE department(
+    id INTEGER NOT NULL, 
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+    );
+
+
+CREATE TABLE role(
+   id INTEGER  NOT NULL,
+   title VARCHAR(30),
+   salary DECIMAL,
+   department_id INT NOT NULL,
+   PRIMARY KEY (id)
+);
+
+CREATE TABLE employee(
+id INTEGER NOT NULL,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT NOT NULL,
+manager_id INT,
+PRIMARY KEY(id)
+);
