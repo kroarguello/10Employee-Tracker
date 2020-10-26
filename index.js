72e044d0952efd30d0c31fca9704c6f58f572426
Delmy Arguello <kroarguelloadel@gmail.com>
2020-10-26 18:54:58 -0500

    adding department in index

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const askEmployee= require("./questions/askEmployee.js");
 
+const inquirer = require("inquirer");
 const mysql = require("mysql");
+
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -10,11 +11,87 @@ const connection = mysql.createConnection({
   database: "department_db" //Data Base 
 });
 
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    askEmployee();
-  });
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  askEmployee();
+ });
+
+
+
+function askEmployee() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "question",
+        message: "What do you like to do",
+        choices: [
+          "View Employee",
+          "Add Employee",
+          "Update Employee",
+          "Delete Employee",
+          "Add department",
+          "Add Role",
+          "View Budget by Department",
+          "Exit"
+        ]
+
+      }
+    ])
   
-  
+    .then(answer => {
+
+      if (answer.question === "View Employee") {
+        viewEmployee();
+      }
+
+      if (answer.question === "Add Employee") {
+        addEmployee();
+      }
+
+      if (answer.question === "Update Employee") {
+        updateEmployee();
+      }
+
+      if (answer.question === "Delete Employee") {
+        deleteEmployee();
+      }
+
+      if (answer.question === "Add department") {
+        addDepartment();
+      }
+
+      if (answer.question === "Add Role") {
+        addRole();
+      }
+
+      if (answer.question === "View Budget by Department") {
+        viewBudget();
+      }
+
+      if (answer.question === "Exit") {
+        //console.log("Thank you see you next time");
+        connection.end();
+      }
+    })
+}
+
+
+async function addDepartment() {
+  const department = await inquirer.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Name of the department"
+      }
+    ])
+        
+        connection.query("INSERT INTO department (name) VALUES ('" + department.name + "')", (err, res) =>{
+        if (err) throw err;
+      })
+}
+
+
+ 
   
